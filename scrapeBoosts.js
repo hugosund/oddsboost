74625bf3ddf0f26b05ca9c9641d423ac14d2dccd
hugosund <61327930+hugosund@users.noreply.github.com>
2026-01-04 22:01:34 +0100

    Update scrapeBoosts.js

--- scrapeBoosts.js
+++ scrapeBoosts.js
@@ -1,31 +1,16 @@
-
-const fs = require("fs");
-const puppeteer = require("puppeteer-core");
-const ftp = require("basic-ftp");
-
-const CHROME_PATH =
-  process.env.PUPPETEER_EXECUTABLE_PATH ||
-  "/usr/bin/google-chrome-stable" ||
-  "/usr/bin/chromium";
+const puppeteer = require("puppeteer");
 
 (async () => {
   console.log("Startar Puppeteer…");
 
   const browser = await puppeteer.launch({
-    executablePath: CHROME_PATH,
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage"
-    ]
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
 
   const page = await browser.newPage();
-  await page.goto("https://example.com", { timeout: 60000 });
-  console.log("Chromium fungerar");
+  await page.goto("https://example.com", { waitUntil: "domcontentloaded" });
+
+  console.log("Puppeteer fungerar ✅");
 
   await browser.close();
-
-  fs.writeFileSync("boosts.html", "<h1>OK</h1>");
 })();
